98d2cfc3c7a581297947422eb5d3cc225cad7210
MAdriaansens <132375271+MAdriaansens@users.noreply.github.com>
2023-06-03 21:47:25 +0200

    Create check_for_repeats_in_genome_list.py

---
+++ check_for_repeats_in_genome_list.py
@@ -0,0 +1,15 @@
+import os
+import pandas as pd
+import subprocess
+df = pd.read_csv('Euk5_genome_download_edit.csv', sep = ',')
+genome_name= list(df['genome_name'])
+index =df.set_index('genome_name', inplace=True)
+Genome= './databases/Eukarya5/genomes/'
+abbreviation_pair = df.loc[genome_name]['Abbreviation']
+#if you wish to count the number of overlaps use count = count+1
+count = 0
+for i in genome_name:
+    for genome in os.listdir(Genome):
+        if i == genome in os.listdir(Genome):
+#to run the entire process
+       #     subprocess.run(['echo',databases/Eukarya5/genomes/genome, abbreviation_pair[i]])
